a44d6fe64b7fd78d36b5059170c580225093e005
ag1119 <abhiguptacool2050@gmail.com>
2019-03-02 10:16:50 +0530

    bug fixes in add tab layout and fragment

--- app/src/main/java/com/example/anticorruptionapp/MainActivity.java
+++ app/src/main/java/com/example/anticorruptionapp/MainActivity.java
@@ -1,13 +1,29 @@
 package com.example.anticorruptionapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.anticorruptionapp.Home.HomePage;
+
+public class MainActivity extends AppCompatActivity implements Runnable {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Thread main = new Thread(MainActivity.this);
+        main.start();
+    }
+
+    @Override
+    public void run() {
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        finish();
+        startActivity(new Intent(MainActivity.this , HomePage.class));
     }
 }
