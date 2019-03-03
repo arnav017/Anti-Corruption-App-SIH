956f5e71e414e14f0ae4352400dad4d7404ff858
goyalakash1997 <goyalakash1997@gmail.com>
2019-03-03 14:53:22 +0530

    thirCommitfromdevakash

--- app/src/main/java/com/example/anticorruptionapp/SignIn.java
+++ app/src/main/java/com/example/anticorruptionapp/SignIn.java
@@ -1,6 +1,7 @@
 package com.example.anticorruptionapp;
 
 import android.app.ProgressDialog;
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -8,6 +9,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.example.anticorruptionapp.Home.HomePage;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.FirebaseException;
@@ -41,6 +43,9 @@ public class SignIn extends AppCompatActivity {
         super.onStart();
         if(firebaseUser!=null){
             Toast.makeText(this,"Already Login",Toast.LENGTH_LONG).show();
+            Intent intent=new Intent(SignIn.this,HomePage.class);
+            startActivity(intent);
+            finish();
         }
         else
             Toast.makeText(this,"Not Login",Toast.LENGTH_LONG).show();
@@ -113,6 +118,9 @@ public class SignIn extends AppCompatActivity {
                             //here you can open new activity
                             Toast.makeText(getApplicationContext(),
                                     "Login Successfull", Toast.LENGTH_LONG).show();
+                            Intent intent=new Intent(SignIn.this, HomePage.class);
+                            startActivity(intent);
+                            finish();
                         } else {
                             if (task.getException() instanceof FirebaseAuthInvalidCredentialsException) {
                                 Toast.makeText(getApplicationContext(),
