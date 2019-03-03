f9857357eab6471d71ed18480dae5c1b910440c1
goyalakash1997 <goyalakash1997@gmail.com>
2019-03-03 13:22:34 +0530

    thirdCommitfromdevakash

---
+++ app/src/main/java/com/example/anticorruptionapp/Model.java
@@ -0,0 +1,94 @@
+package com.example.anticorruptionapp;
+
+
+public class Model {
+    String key , contact , name , email , designation , home_address , home_district , work_address , work_district,state;
+    String image_uri;
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public String getContact() {
+        return contact;
+    }
+
+    public void setContact(String contact) {
+        this.contact = contact;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getDesignation() {
+        return designation;
+    }
+
+    public void setDesignation(String designation) {
+        this.designation = designation;
+    }
+
+    public String getHome_address() {
+        return home_address;
+    }
+
+    public void setHome_address(String home_address) {
+        this.home_address = home_address;
+    }
+
+    public String getHome_district() {
+        return home_district;
+    }
+
+    public void setHome_district(String home_district) {
+        this.home_district = home_district;
+    }
+
+    public String getWork_address() {
+        return work_address;
+    }
+
+    public void setWork_address(String work_address) {
+        this.work_address = work_address;
+    }
+
+    public String getWork_district() {
+        return work_district;
+    }
+
+    public void setWork_district(String work_district) {
+        this.work_district = work_district;
+    }
+
+    public String getImage_uri() {
+        return image_uri;
+    }
+
+    public void setImage_uri(String image_uri) {
+        this.image_uri = image_uri;
+    }
+    public void setState(String state){
+        this.state=state;
+    }
+
+    public String getState() {
+        return state;
+    }
+}
